c70eb67318830282540a0a08fcf30dc4290ba2cb
Apisitkitti <apisit.o@mail.kmutt.ac.th>
2024-01-28 21:54:19 +0700

    card_finish

--- src/components/object_component/CardContent.tsx
+++ src/components/object_component/CardContent.tsx
@@ -1,5 +1,9 @@
-export default function Card({ content_pic, imageIsNotLoad, typeOfInformation, restuarant_name, restuarant_place }
-  : { content_pic?: string, imageIsNotLoad: string, typeOfInformation?: string, restuarant_name: string, restuarant_place: string }) {
+export default function Card({ content_pic, imageIsNotLoad, typeOfInformation, restuarant_name, restuarant_place, restuarant_score, reviewer_number, food_category_first,
+  food_category_second, food_category_final }
+  : {
+    content_pic?: string, imageIsNotLoad: string, typeOfInformation?: string, restuarant_name: string, restuarant_place: string, restuarant_score: number,
+    reviewer_number: number, food_category_first?: string, food_category_second?: string, food_category_final?: string
+  }) {
   return (
     <>
       <div className="pic_content_container">
@@ -17,9 +21,16 @@ export default function Card({ content_pic, imageIsNotLoad, typeOfInformation, r
           <img src="./img/bookmark-white_icon.png" alt="bookmark_icon" />
         </div>
         <div className="title_second_line">
-
+          <div className="rating_label">
+            <span id="rating_show">{restuarant_score}</span> &starf;
+          </div>
+          <span id="review">{reviewer_number} รีวิว</span>
         </div>
-        <div></div>
+        <div>
+          <a href="#">{food_category_first}</a>,
+          <a href="#"> {food_category_second}</a>,
+          <a href="#"> {food_category_final}</a>
+        </div>
         <div></div>
       </div>
     </>
